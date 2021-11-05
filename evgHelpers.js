350c343828b20f66fd1a8e67428bdfe33526bd7b
Sam Bessey <sam@sambessey.com>
2021-11-05 14:32:00 +1100

    Added Querystring helper

--- evgHelpers.js
+++ evgHelpers.js
@@ -15,6 +15,23 @@ var evgHelpers = {
     capThis: function(w) {
         if (w) return [w.charAt(0).toUpperCase() + w.slice(1)]
     },
+    //simpleURLParam consumes URL parameters and returns the parameter found at a specific position
+    //NOTE: THIS ASSUMES THAT THE ORDER OF URL PARAMS IS CONSISTENT - IT DOES NOT TAKE THE KEY INTO ACCOUNT 
+    //OR MATCH THE KEY TO AN ATTRIBUTE!
+    //Use this in config.global.onActionEvent as follows:
+    //const qsMapping = { source:evgHelpers.domain(0),utmtags:evgHelpers.urlParam(1)}
+    //event.user = event.user || {};
+    //event.user.attributes = event.user.attributes || {};
+    //event.user.attributes.camefrom = qsMapping.domain;
+    //event.user.attributes.campaigntags = qsMapping.utmtags;
+
+    simpleURLParam: function (pos) {
+        let qs = window.location.href.split('?')[1].split("&")
+        qs.forEach(function(part, i) {
+            this[i] = this[i].substring(this[i].indexOf("=") + 1)
+        }, qs);
+        return qs[pos];
+},
     //findBiggestImage returns the src of the biggest image found on the page. Useful for catalog building.
     //There is a filer on this example of images that have an src containing 'hover'. This is to 
     //differentiate  product images from others on the page. You will need to adjust this to reflect
@@ -33,6 +50,36 @@ var evgHelpers = {
                 }
         }
         if (maxImage) return (maxImage.src);
+    },
+    //waitForElement waits for a specific element to appear before trying to do anything with it. This is the recommended approach from Salesforce/ Evergage
+    // in the event of an element not appearing immediately as it is non-blocking. It returns a promise: 
+    //// - resolved if the element is found within x time
+    //// - rejected if the element does not appear in time
+    // Use the following code in your sitemap to call this function:
+    /*
+    evgHelpers.waitForElement("#nn-vue-fieldid-phoneNumber")
+    .then((res)=>{/*Do something now we have the value*//*})
+    .catch((error) => {console.log('error '+error)});
+        */
+    waitForElement: function(el) {    
+        return new Promise((resolve, reject) => {
+            let c=0
+            let int = setInterval(_=>{
+                if (document.querySelector(el)) {
+                    clearInterval(int)
+                    let i = el
+                        resolve(i)
+                }
+                else {
+                    c < 100 && c++
+                    console.log('plus',c)
+                    if(c >= 10) {
+                        clearInterval(int)
+                            reject('We didnt see the element and waited long enough.')
+                    }
+                }
+            },100)
+        });
     },
     //getDatalayer waits for the datalayer to become accessible to the browser, then it returns
     //a resolved promise that contains the Krux DL object. It tries 10 times then gives up if not found
@@ -72,6 +119,8 @@ var evgHelpers = {
         console.log(`Found something we didn't account for: ${typeof DL[value]} is a ${value}`)
     }
 }
+//END OF HELPERS
+/////////////////////////////////////////////////////
 
 //DATALAYER EXAMPLE
 //THE BELOW IS AN EXAMPLE OF USING MULTIPLE HELPERS FROM ABOVE TO POPULATE THE CATALOG. 
